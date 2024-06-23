7cbc2a7ee228b01166c347eaad4e050ae730ecdd
sofiviera <sofiavierasosa@gmail.com>
2024-06-23 19:18:41 -0300

    ErrorMsg en profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,12 +6,13 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { storeTweets } from "../redux/tweetSlice";
 import Tweet from "../components/Tweet";
+import ErrorMsg from "../components/ErrorMsg";
 
 const Profile = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const allTweets = useSelector((state) => state.tweets);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const getUserTweets = async () => {
@@ -31,45 +32,49 @@ const Profile = () => {
 
   return (
     <>
-      <div className="row">
-        <div className="col-3">
-          <SideBar />
-        </div>
-        <div className="col-6 text-white">
-          <div className="mb-4" id="profile-background-image">
-            <img
-              id="profile-image"
-              className="rounded-circle"
-              style={{ width: 50, height: 50 }}
-              src={
-                user.image
-                  ? user.image
-                  : "https://www.shutterstock.com/image-vector/young-smiling-man-avatar-brown-600nw-2261401207.jpg"
-              }
-            />
+      {user ? (
+        <div className="row">
+          <div className="col-3">
+            <SideBar />
           </div>
-          <div className="d-flex justify-content-between">
-            <h5 className="">{user.firstname}</h5>
-
-            <div>
-              <span>Following 10</span> <span>Followers 20</span>
+          <div className="col-6 text-white">
+            <div className="mb-4" id="profile-background-image">
+              <img
+                id="profile-image"
+                className="rounded-circle"
+                style={{ width: 50, height: 50 }}
+                src={
+                  user.image
+                    ? user.image
+                    : "https://www.shutterstock.com/image-vector/young-smiling-man-avatar-brown-600nw-2261401207.jpg"
+                }
+              />
             </div>
+            <div className="d-flex justify-content-between">
+              <h5 className="">{user.firstname}</h5>
+
+              <div>
+                <span>Following 10</span> <span>Followers 20</span>
+              </div>
+            </div>
+            <p>@{user.username}</p>
+            <p>
+              {user.description ? user.description : "Agrega una biografia..."}
+            </p>
+            <div className="mb-4">
+              <span className="border-bottom border-info border-3">Tweets</span>
+            </div>
+            {allTweets.map((tweet) => (
+              <Tweet key={tweet._id} tweet={tweet} />
+            ))}
           </div>
-          <p>@{user.username}</p>
-          <p>
-            {user.description ? user.description : "Agrega una biografia..."}
-          </p>
-          <div className="mb-4">
-            <span className="border-bottom border-info border-3">Tweets</span>
+          <div className="col-3">
+            <Trending />
           </div>
-          {allTweets.map((tweet) => (
-            <Tweet key={tweet._id} tweet={tweet} />
-          ))}
         </div>
-        <div className="col-3">
-          <Trending />
-        </div>
-      </div>
+      ) : (
+        <ErrorMsg />
+      )}
     </>
   );
 };
